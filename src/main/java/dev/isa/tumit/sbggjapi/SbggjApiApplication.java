28e1fdde6444661bcd206d9853d86f0eed22ef5f
tumit <tumit@live.com>
2024-01-07 00:56:17 +0700

    feat: init project

---
+++ src/main/java/dev/isa/tumit/sbggjapi/SbggjApiApplication.java
@@ -0,0 +1,13 @@
+package dev.isa.tumit.sbggjapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbggjApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbggjApiApplication.class, args);
+	}
+
+}
